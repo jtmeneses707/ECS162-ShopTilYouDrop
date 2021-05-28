cda356241444d414262ff977b77513f44c9b169c
Sang Nguyen <>
2021-05-28 15:55:42 +0000

    Fixed the bug in shop4.

--- src/layout/shop4.jsx
+++ src/layout/shop4.jsx
@@ -36,7 +36,7 @@ function Shop4(props) {
 
   //  Fetching data from server...
   useAsyncFetch(`/api/get-school-discount?schoolName=${schoolName}&range=${range}`, {}, thenFun, catchFun);
-  function thenFun (result) {
+  function thenFun(result) {
     console.log(result);
     setTuition(result.tuition);
     setFee(result.other);
@@ -46,7 +46,7 @@ function Shop4(props) {
     setLocation(result.location);
     setLoading(false)
   }
-  function catchFun (error) {
+  function catchFun(error) {
     console.log(error);
   }
 
@@ -55,90 +55,94 @@ function Shop4(props) {
       <BlueHeader> TOTAL </BlueHeader>
       <img id="image-shop4" src="./img/receipt.png" />
       <div className="main-subtitile-shop4">
-        <div className = "table-wrapper"> 
-        <div className = "title-wrapper-shop4"> 
-        <span className="special-text">{schoolName.toUpperCase()}</span><br/>
-        <span>{location.toUpperCase()}</span>
+        <div className="table-wrapper">
+          <div className="title-wrapper-shop4">
+            <span className="special-text">{schoolName.toUpperCase()}</span><br />
+            <span>{location.toUpperCase()}</span>
+          </div>
         </div>
-        </div>
-        <div className = "table-wrapper1"> 
-        <div id="table">
-          <table>
-            <tr className="tabletitle">
-              <td className="item"><h2 >QTY</h2></td>
-              <td className="des"><h2>DESCRIPTION</h2></td>
-              <td className="amount"><h2>AMOUNT</h2></td>
-            </tr>
-            <tr className="service">
-              <td className="tableitem"><p className
-                ="itemtext">1</p></td>
-              <td className
-                ="tableitem"><p className="itemtext" className
-                ="text1">TUITION</p></td>
-              <td className
-                ="tableitem"><p className
-                  ="itemtext">${tuition.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</p>
-              </td>
-            </tr>
-            <tr className
-              ="service">
-              <td className
-                ="tableitem"><p className
-                  ="itemtext">1</p></td>
-              <td className
-                ="tableitem"><p className
-                  ="itemtext" className
-                ="text1">FEES, SUPPLIES AND LIVING EXPENSES</p></td>
-              <td className
-                ="tableitem"><p className
-                  ="itemtext">${fee.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</p></td>
-            </tr>
-            <tr className
-              ="service">
-              <td></td>
-              <td className
-                ="tableitem"><p className
-                  ="itemtext" className
-                ="text2">SUBTOTAL</p></td>
-              <td className
-                ="tableitem"><p className
-                  ="itemtext">${totalFee.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</p></td>
-            </tr>
+        <div className="table-wrapper1">
+          <div id="table">
+            <table>
+              <tbody>
+                <tr className="tabletitle">
+                  <td className="item"><h2 >QTY</h2></td>
+                  <td className="des"><h2>DESCRIPTION</h2></td>
+                  <td className="amount"><h2>AMOUNT</h2></td>
+                </tr>
+                <tr className="service">
+                  <td className="tableitem"><p className
+                    ="itemtext">1</p></td>
+                  <td className
+                    ="tableitem"><p className="itemtext" className
+                      ="text1">TUITION</p></td>
+                  <td className
+                    ="tableitem"><p className
+                      ="itemtext">${tuition.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</p>
+                  </td>
+                </tr>
+                <tr className
+                  ="service">
+                  <td className
+                    ="tableitem"><p className
+                      ="itemtext">1</p></td>
+                  <td className
+                    ="tableitem"><p className
+                      ="itemtext" className
+                      ="text1">FEES, SUPPLIES AND LIVING EXPENSES</p></td>
+                  <td className
+                    ="tableitem"><p className
+                      ="itemtext">${fee.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</p></td>
+                </tr>
+                <tr className
+                  ="service">
+                  <td></td>
+                  <td className
+                    ="tableitem"><p className
+                      ="itemtext" className
+                      ="text2">SUBTOTAL</p></td>
+                  <td className
+                    ="tableitem"><p className
+                      ="itemtext">${totalFee.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</p></td>
+                </tr>
+              </tbody>
             </table>
             <div id="table-total">
-            <table>
-            <tr className
-              ="tabletitle">
-              <td className
-                ="discount"><p>DISCOUNT</p></td>
-              <td className="blank"></td>
-              <td className
-                ="payment"><p>-${discount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</p></td>
-            </tr>
-            <tr className
-              ="tabletitle">
-              <td className
-                ="total-text"><p>TOTAL</p></td>
-              <td></td>
-              <td className
-                ="total"><p>${totalWithDiscount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}*</p></td>
-            </tr>
-            </table>
+              <table>
+                <tbody>
+                  <tr className
+                    ="tabletitle">
+                    <td className
+                      ="discount"><p>DISCOUNT</p></td>
+                    <td className="blank"></td>
+                    <td className
+                      ="payment"><p>-${discount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</p></td>
+                  </tr>
+                  <tr className
+                    ="tabletitle">
+                    <td className
+                      ="total-text"><p>TOTAL</p></td>
+                    <td></td>
+                    <td className
+                      ="total"><p>${totalWithDiscount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}*</p></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
-        </div>
-        <div className = "table-wrapper"> 
-        <div id="legalcopy">
-          <div> <p>* </p></div>
-          <div> 
-          <p className
-            ="legal"> This estimate is based on actual costs for families that received federal aid or loans by applying with the FAFSA form. It always pays to ask for a discount!</p>
-          <p className
-            ="legal"> Cost includes tuition, living costs, books and supplies, and fees minus the average grants and scholarships for federal financial aid recipients. </p>
-          <p className
-            ="legal"> Depending on the feral state, or institutional grant aid available, students in your income bracket may pay more or less than the overall average costs. </p>
+        <div className="table-wrapper">
+          <div id="legalcopy">
+            <div> <p>* </p></div>
+            <div>
+              <p className
+                ="legal"> This estimate is based on actual costs for families that received federal aid or loans by applying with the FAFSA form. It always pays to ask for a discount!</p>
+              <p className
+                ="legal"> Cost includes tuition, living costs, books and supplies, and fees minus the average grants and scholarships for federal financial aid recipients. </p>
+              <p className
+                ="legal"> Depending on the feral state, or institutional grant aid available, students in your income bracket may pay more or less than the overall average costs. </p>
+            </div>
           </div>
-        </div>
         </div>
       </div>
       <Link to="/shop1" style={{ textDecoration: 'none' }}>
@@ -146,10 +150,10 @@ function Shop4(props) {
       </Link>
     </React.Fragment>
   )
-  
+
   return (
     <div className="main-container">
-      {loading ? <p>Loading...</p> :  content}
+      {loading ? <p>Loading...</p> : content}
     </div>
   )
 }
